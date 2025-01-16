935528750da0642e42007a389adbfa7db50e2b54
teycheng <pagnavathtey@gmail.com>
2025-01-16 00:45:12 -0800

    fixed camera
    make sure position is 0,10,-10
    and rotation is 45,0,0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-//imports 
+
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
-
+    // Rigidbody of the player.
     private Rigidbody rb;
+
+    // Movement along X and Y axes.
     private float movementX;
     private float movementY;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Speed at which the player moves.
+    public float speed = 0;
+
+    // Start is called before the first frame update.
     void Start()
     {
+        // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
     }
 
+    // This function is called when a move input is detected.
     void OnMove(InputValue movementValue)
     {
+        // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
 
+        // Store the X and Y components of the movement.
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
-   
-    void FixedUpdate()
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
     {
+        // Create a 3D movement vector using the X and Y inputs.
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+        // Apply force to the Rigidbody to move the player.
         rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
 }
